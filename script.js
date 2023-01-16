5066b77b017522e886bb1cb495230ee664cf606e
GayanMartirosyan777 <114398638+GayanMartirosyan777@users.noreply.github.com>
2023-01-16 17:21:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// 1
+// var minute = 28;
+// if (minute >= 0 && minute<=14){
+//     console.log("В первую четверть. ");
+// }
+// if (minute >= 15 && minute<=30){
+//     console.log("Во вторую четверть. ");
+// }
+// if (minute >= 31 && minute<=45){
+//     console.log("В третью четверть. ");
+// }
+// if (minute >= 46 && minute<=59){
+//     console.log("В четвертую четверть. ");
+// }
+
+// 5
+//  var i = 1
+//  while (i <= 12) {
+//     console.log(i);
+//     i++
+//   }
+
+// 6
+// var i = 5
+//  while (i <= 13) {
+//     console.log(i);
+//     i++
+//   }
+
+// 7
+// for (let i = 0; i <= 16; i++)
+//     if(i % 2 == 0) {
+//         console.log(i);
+//     }
+
+//8
+let array = [1,2,3,4,5];
+let result = 0;
+for (let i = 0; i < array.length; i++) {
+    result += frrfy[i];
+}
+console.log(result);
